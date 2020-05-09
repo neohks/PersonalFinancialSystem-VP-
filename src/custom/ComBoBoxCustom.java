60e3de60c19d5758affd2eb6deb296c9337f5498
Neoh Kim Seng <neohks21@gmail.com>
2020-05-09 21:10:39 +0800

    + Add Custom ComBoBox for scroll vertically

---
+++ src/custom/ComBoBoxCustom.java
@@ -0,0 +1,90 @@
+package custom;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.plaf.basic.BasicComboPopup;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+import javax.swing.plaf.basic.ComboPopup;
+
+public class ComBoBoxCustom {
+
+    public static JComboBox<String> comboBox = new JComboBox<String>();
+    
+    public ComBoBoxCustom() {
+       
+        comboBox.addItem("January");
+        comboBox.addItem("February");
+        comboBox.addItem("March");
+        comboBox.addItem("April");
+        comboBox.addItem("May");
+        comboBox.addItem("June");
+        comboBox.addItem("July");
+        comboBox.addItem("August");
+        comboBox.addItem("September");
+        comboBox.addItem("October");
+        comboBox.addItem("November");
+        comboBox.addItem("December");
+        
+
+        comboBox.setUI(new BasicComboBoxUI() {
+            @Override
+            protected ComboPopup createPopup() {
+                return new BasicComboPopup(comboBox) {
+                    @Override
+                    protected JScrollPane createScroller() {
+                        JScrollPane scroller = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                        scroller.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
+                            @Override
+                            protected JButton createDecreaseButton(int orientation) {
+                                return createZeroButton();
+                            }
+
+                            @Override
+                            protected JButton createIncreaseButton(int orientation) {
+                                return createZeroButton();
+                            }
+
+                            @Override
+                            public Dimension getPreferredSize(JComponent c) {
+                                return new Dimension(10, 5);
+                            }
+
+                            private JButton createZeroButton() {
+                                return new JButton() {
+                                    @Override
+                                    public Dimension getMinimumSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+
+                                    @Override
+                                    public Dimension getPreferredSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+
+                                    @Override
+                                    public Dimension getMaximumSize() {
+                                        return new Dimension(new Dimension(0, 0));
+                                    }
+                                };
+                            }
+                        });
+                        return scroller;
+                    }
+                };
+            }
+        });
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(comboBox);
+
+    }
+}
